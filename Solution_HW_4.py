678e49fe9fead69aa2a6849ec5a87ce4143b4886
mulajnoah <39249597+mulajnoah@users.noreply.github.com>
2018-06-11 20:22:22 +0200

    Add files via upload

---
+++ Solution_HW_4.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class Agent:
+    def __init__(self):
+         x_coordinate = np.random.coordinate[0,25]
+         y_coordinate = np.random.coordinate[0,25]
+        self coordinations = (x_coordinate,y_coordinate)
+        self.history = []
+        
+        def move(self):
+            ***Move the agent***
+            x_coordinate = self.coordinate[0]
+            y_coordinate = self.coordinate[1]
+            x_coordinate += np.random.randint(-1,1)
+            y_coordinate += np.random.randint(-1,1)
+***self.update_coordinate(x_coordinate, y_coordinate)
+
+***return x_coordinate, y_coordinate
+
+def update_coordinated(self,x_coordinate,y_coordinate):
+***Update current agent position***
+self_history_append(self.coordinate)
+self.coordinate = (x_coordinate, y_coordinate)            
+
+class Model:
+    def __init__(self):
+        self.agents = []
+        for_in range(10)
+        self.agents.append(Agent())
+        
+    
+    print(my_object.coordinate)
+if__name_=="_main_":
+    for agent in my_model.agents:
+        *** print (agent.coordinate)
+        agent.move()
+        print(agent.history)
+        print(agent.coordinate)
+        
+        
+    my_object = Agent()
+    my_object.coordinate
+    my_obhect.move()
+    my_object.coordinate
