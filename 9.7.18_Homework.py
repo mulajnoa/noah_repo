45d3e2cee9ec3268eada4627aa3e42566fb96884
mulajnoah <39249597+mulajnoah@users.noreply.github.com>
2018-07-09 11:51:39 +0200

    Homework_9.7.18
    Homework_9.7.18

---
+++ 9.7.18_Homework.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 16:45:07 2018
+
+@author: naimm
+"""
+import numpy as np
+import matplotlib.pylab as plt
+x = np.linspace(-50,50,100, endpoint=TRUE)
+
+F1=[]
+for i in X:
+    if i<= -25:
+        F1 append(np.sin(X*np.random.randint(7,size=10)))
+        
+    if i > -25 and i<=0:
+        F1 append(np.sin(X*np.random.randint(15,size=10)))
+        if i > 0 and i <=25
+        F1 append(np.sin(X*np.random.randint(28,size=10)))
+    if>0 and i<=25
+        F1 append(np.sin(X*np.random.randint(49,size=10)))
+    if > 25 and i<=50:
+             
+        F1.append(np.sin(X*np.random.randint(49, size=10)))
+             
+print(F1)
+         
+
+
+    
